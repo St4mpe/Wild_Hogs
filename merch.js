4adf8a8cb6b86a2884cf38364e50a8505889b405
St4mpe <hampusekmansundin@gmail.com>
2025-12-02 15:31:06 +0100

    1

--- merch.js
+++ merch.js
@@ -1,18 +1,67 @@
-const grids = document.querySelectorAll('.gridBox');
-const overlay = document.getElementById('overlay');
+document.addEventListener("DOMContentLoaded", function(){
+  const grids = document.querySelectorAll('.gridBox');
 
-grids.forEach(grid => {
-  const priceEl = grid.querySelector('.price');
-  priceEl.textContent = grid.dataset.price;
+  const hoodie = document.getElementById('hoodie');
+  const helmet = document.getElementById('helmet');
+  const stekpanna = document.getElementById('stekpanna');
+  const gryta = document.getElementById('gryta');
+  const knivset = document.getElementById('knivset');
+  const gun = document.getElementById('gun');
 
-  grid.addEventListener('mouseenter', () => {
-    grid.classList.add('active');
-    overlay.classList.add('visible');
+  const overlay = document.getElementById('overlay');
+  const totalSumma = document.getElementById('total');
+
+  let sum = 0;
+
+  grids.forEach(grid => {
+    const priceEl = grid.querySelector('.price');
+    priceEl.textContent = grid.dataset.price;
+
+    grid.addEventListener('mouseenter', () => {
+      grid.classList.add('active');
+      overlay.classList.add('visible');
+    });
+
+    grid.addEventListener('mouseleave', () => {
+      grid.classList.remove('active');
+      overlay.classList.remove('visible');
+    });
   });
 
-  grid.addEventListener('mouseleave', () => {
-    grid.classList.remove('active');
-    overlay.classList.remove('visible');
-  });
-});
 
+  hoodie.addEventListener("click", () => {
+      sum += 550;
+      console.log(sum);
+      totalSumma.innerHTML = sum+"kr";
+  })
+
+  helmet.addEventListener("click", () => {
+      sum += 750;
+      console.log(sum);
+      totalSumma.innerHTML = sum+"kr";
+  })
+
+  stekpanna.addEventListener("click", () => {
+      sum += 450;
+      console.log(sum);
+      totalSumma.innerHTML = sum+"kr";
+  })
+
+  gryta.addEventListener("click", () => {
+      sum += 350;
+      console.log(sum);
+      totalSumma.innerHTML = sum+"kr";
+  })
+
+  knivset.addEventListener("click", () => {
+      sum += 1050;
+      console.log(sum);
+      totalSumma.innerHTML = sum+"kr";
+  })
+
+  gun.addEventListener("click", () => {
+      sum += 2500;
+      console.log(sum);
+      totalSumma.innerHTML = sum+"kr";
+  })
+})
